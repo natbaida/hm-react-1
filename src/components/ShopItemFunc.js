f769f3316a336266ad81bbb09eb03eae2319d65a
Natalia Baida <nnbaida@yandex.com>
2025-11-28 13:44:49 +0300

    initial commit

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,37 @@
+export default function ShopItemFunc({ item }) {
+  const {
+    brand,
+    title,
+    description,
+    descriptionFull,
+    price,
+    currency
+  } = item;
+
+  const formattedPrice = price.toFixed(2);
+
+  return (
+    <div className="main-content">
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+
+      <div className="description">
+        {descriptionFull}
+      </div>
+
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+
+      <div className="divider"></div>
+
+      <div className="purchase-info">
+        <div className="price">
+          {currency}{formattedPrice}
+        </div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
